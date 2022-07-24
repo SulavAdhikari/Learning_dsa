888091e3c4a7c1072d86651ae9e98b5b340577a0
sulavadhikari <cabbage.adhikari18@gmail.com>
2022-07-24 22:32:24 +0545

    made changes in singly ll

---
+++ linkedlist/linked_list.py
@@ -0,0 +1,43 @@
+# A single node of a singly linked list
+class Node:
+  # constructor
+  def __init__(self, data = None, next=None): 
+    self.data = data
+    self.next = next
+
+# A Linked List class with a single head node
+class LinkedList:
+  def __init__(self):  
+    self.head = None
+  
+  # insertion method for the linked list
+  def insert(self, data):
+    newNode = Node(data)
+    if(self.head):
+      current = self.head
+      while(current.next):
+        current = current.next
+      current.next = newNode
+    else:
+      self.head = newNode
+  
+class LLoutput():
+  
+  def __init__(self, linkedlist):
+    self.linkedlist = linkedlist
+
+    # print method for the linked list
+  def print_ll(self,):
+    current = self.linkedlist.head
+    while(current):
+      print(current.data)
+      current = current.next
+
+# Singly Linked List with insertion and print methods
+LL = LinkedList()
+LL.insert(3)
+LL.insert(4)
+LL.insert(5)
+LL_output = LLoutput(LL)
+
+LL_output.print_ll()
